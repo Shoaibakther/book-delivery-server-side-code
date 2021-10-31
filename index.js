f7663535a39d0bc91fa7c98874f52bcfeb18dd24
shoaibakther <shoaibakthershopno@gmail.com>
2021-10-31 17:33:27 +0600

    updeted

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
                 const result = await ordersCollection.find({ email: req.params.email }).toArray();
                 res.send(result);
             })
+            // Delete Order 
+            app.delete("/myOrders/:id", async (req, res) => {
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)}
+                const result = await ordersCollection.deleteOne(query);
+                console.log(result);
+            })
         })
     } finally {
         // await client.close();
